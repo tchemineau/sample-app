cafeaa677271ab7fd06f95f83447db1cbc909776
thomas <thomas.chemineau@gmail.com>
2013-04-26 15:56:34 +0200

    add a route to the API

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -132,6 +132,10 @@ Cookie::$salt = 'foobar';
  * Set the routes. Each route must have a minimum of a name, a URI and a set of
  * defaults for the URI.
  */
+Route::set('rest_v1', 'api/v1/(<controller>(/<action>(/<id>)))')
+	->defaults(array(
+		'directory'  => 'RESTv1'
+	));
 Route::set('default', '(<controller>(/<action>(/<id>)))')
 	->defaults(array(
 		'controller' => 'welcome',
