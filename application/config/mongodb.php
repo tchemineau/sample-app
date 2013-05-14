a4a79094fcb34402481838fc84b0ecc5a8619313
thomas <thomas.chemineau@gmail.com>
2013-05-14 19:03:56 +0200

    configure mongodb defualt database

---
+++ application/config/mongodb.php
@@ -0,0 +1,11 @@
+<?php defined('SYSPATH') or die();
+
+return array(
+	'default' => array(
+		'host' => 'localhost',
+		'port' => 27017,
+		'database' => 'sample-app',
+		'username' => '',
+		'password' => '',
+	),
+);
