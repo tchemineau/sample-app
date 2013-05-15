40abf69ec4854ec88ee642ca17b17ead9792ddff
thomas <thomas.chemineau@gmail.com>
2013-05-15 17:43:30 +0200

    add data validation of account

--- application/classes/Controller/RESTv1/Account.php
+++ application/classes/Controller/RESTv1/Account.php
@@ -6,7 +6,7 @@
 class Controller_RESTv1_Account extends RESTful_Controller
 {
 
-	public function action_get()
+	public function action_get ()
 	{
 		$this->response->body(json_encode(array(
 			'firstname' => 'John',
@@ -14,20 +14,25 @@ class Controller_RESTv1_Account extends RESTful_Controller
 		)));
 	}
 
-	public function action_update()
+	public function action_update ()
 	{
 	}
 
-	public function action_create()
+	public function action_create ()
 	{
+		$data = $this->request->body();
 		$account = Model::factory('App_Account');
-		$account->values($this->request->body());
+
+		if (!$account->validate_data((array) $data))
+			throw HTTP_Exception::factory(400);
+
+		$account->values($data);
 		$account->save();
 
 		$this->response->status(201);
 	}
 
-	public function action_delete()
+	public function action_delete ()
 	{
 	}
 
--- application/classes/Model/App/Account.php
+++ application/classes/Model/App/Account.php
@@ -3,5 +3,27 @@
 class Model_App_Account extends Model
 {
 	public $_reserved = array();
+
+	public static function validate_data ( $data )
+	{
+		$validation = Validation::factory($data)
+
+			// Username
+			->rule('username', 'min_length', array(':value', 4))
+			->rule('username', 'regex', array(':value', '/^[a-zA-Z0-9]+$/iD'))
+
+			// Password
+			->rule('password', 'min_length', array(':value', 8))
+
+			// Mail address
+			->rule('email', 'email')
+
+			// Firstname, lastname, displayname and pseudo
+			->rule('firstname', 'regex', array(':value', '/[-\w.,+]+/'))
+			->rule('lastname', 'regex', array(':value', '/[-\w.,+]+/'));
+
+		return $validation->check();
+	}
+
 }
 
