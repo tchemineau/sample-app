2a09be37c3aa00ad594fab94fd1423b641b1eb30
thomas <thomas.chemineau@gmail.com>
2013-05-21 15:57:16 +0200

    validation is performed by the view and could display tooltip or alert

--- public/assets/app/module/account/view/createAccount.js
+++ public/assets/app/module/account/view/createAccount.js
@@ -1,39 +1,83 @@
 
 define([
 	'marionette',
+	'marionette.formview',
 	'text!module/account/template/createAccount.html'
-], function(Marionette, CreateAccountTemplate)
+], function(Marionette, MarionetteFormView, CreateAccountTemplate)
 {
-	var CreateAccountView = Marionette.ItemView.extend(
+	var CreateAccountView = Marionette.FormView.extend(
 	{
 		template: CreateAccountTemplate,
 
-		events: {
-			'click #account-create-btn': 'actionCreate'
+                ui: {
+                        email: '#account-email',
+                        password: '#account-password',
+                        firstname: '#account-firstname',
+                        lastname: '#account-lastname'
+                },
+
+		fields: {
+			email: {
+				el: '#account-email',
+				required: "Please enter a valid Email Address.",
+				validations: {
+					email: "Please enter a valid Email Address."
+				}
+			},
+			password: {
+				el: '#account-password',
+				required: "Please enter your password.",
+				validations: {
+					password: "Please enter a valid Password."
+				}
+			}
 		},
 
-		ui: {
-			email: '#account-email',
-			password: '#account-password',
-			firstname: '#account-firstname',
-			lastname: '#account-lastname'
+		rules: {
+			password: function(val)
+			{
+				return /^['a-zA-Z0-9]{8,}$/.test(val);
+			}
 		},
 
-		actionCreate: function (event)
+		onSubmit: function (evt)
 		{
+			evt.preventDefault();
+
+			var data = this.serializeFormData();
+			this.model.set(data);
+
 			var App = this.options.app;
-
-			event.preventDefault();
-			event.stopPropagation();
-
-			this.model.set({
-				'email': this.ui.email.val(),
-				'password': this.ui.password.val(),
-				'firstname': this.ui.firstname.val(),
-				'lastname': this.ui.lastname.val()
-			});
-
 			App.vent.trigger('account:save', this.model);
+		},
+
+		onSubmitFail: function (errors)
+		{
+			var cpt = 0;
+
+			_(errors).each(function (field)
+			{
+				$(field.el+'-control').addClass('error');
+				$(field.el).tooltip({
+					placement: 'right',
+					title: field.error[0]}
+				);
+
+				if (cpt == 0)
+					$(field.el).tooltip('show');
+
+				cpt++;
+			});
+		},
+
+		showErrors: function (errors)
+		{
+			var container = $('#account-alert-container');
+			var alertbox = $('<div class="alert alert-error" />')
+				.append($('<button type="button" class="close" data-dismiss="alert">&times;</button>'))
+				.append('<strong>Error</strong>: Something went wrong');
+
+			alertbox.appendTo(container).alert();
 		}
 	});
 
