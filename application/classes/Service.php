a7cb4e9b52d4940bf28430a5f950e8e88c6bdeb8
thomas <thomas.chemineau@gmail.com>
2013-05-24 18:24:57 +0200

    add Email service

---
+++ application/classes/Service.php
@@ -0,0 +1,25 @@
+<?php defined('SYSPATH') OR die('No direct script access.');
+
+/**
+ * Service base class. All services should extend this class.
+ */
+abstract class Service
+{
+
+	/**
+	 * Create a new service instance.
+	 *
+	 *     $service = Service::factory($name);
+	 *
+	 * @param   string  $name   service name
+	 * @return  Model
+	 */
+	public static function factory ( $name )
+	{
+		// Add the service prefix
+		$class = 'Service_'.$name;
+
+		return new $class;
+	}
+
+} // End Service
---
+++ application/classes/Service/Email.php
@@ -0,0 +1,93 @@
+<?php defined('SYSPATH') or die();
+
+/**
+ * A service class to send emails
+ */
+class Service_Email extends Service
+{
+
+	/**
+	 * Build simple email content
+	 */
+	public static function build_content ( $view, $data = array() )
+	{
+		// Calculate website URL from current request
+		$url = URL::base(Request::current());
+
+		// Load the mail template
+		$content = View::factory('smarty:Mail/'.$view.'.tpl');
+
+		// Set data
+		foreach ($data as $key => $value)
+			$content->set($key, $value);
+
+		// Set calculate variables
+		$content->set('url', $url);
+
+		return $content;
+	}
+
+	/**
+	 * Build simple email headers
+	 */
+	public static function build_headers ( $to, $title, $reply = FALSE, $from = NULL )
+	{
+		if (is_null($from))
+		{
+			$from = array(
+				Kohana::$config->load('config.email_from_address'),
+				Kohana::$config->load('config.email_from_name')
+			);
+		}
+
+		return array(
+			'from' => $from,
+			'to' => $to,
+			'title' => $title,
+			'reply' => $reply
+		);
+	}
+
+	/**
+	 * Send a email
+	 *
+	 * @param {array} $headers
+	 * @param {View} $content
+	 * @return {boolean}
+	 */
+	public static function send ( $headers, $content )
+	{
+		try
+		{
+			// Set headers
+			foreach ($headers as $key => $value)
+				$content->set($key, $value);
+
+			// Instantiate the mailer
+			$mailer = Email::connect();
+
+			// Send the mail
+			$nbmails = Email::send(
+				$headers['to'],
+				$headers['from'],
+				$headers['title'],
+				(string) $content,
+				true, // This is HTML content
+				$headers['reply']
+			);
+
+			return $nbmails > 0;
+		}
+		catch (Exception $e)
+		{
+			Kohana::$log->add(Log::ERROR, "Could not send email to :email\n:error", array(
+				':email' => $headers['to'],
+				':error' => (string) $e
+			));
+
+			return FALSE;
+		}
+	}
+
+}
+
