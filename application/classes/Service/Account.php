934a368d78c543fc1a41c050a050ac2070f78540
Thomas Chemineau <thomas.chemineau@gmail.com>
2013-05-26 15:06:39 +0200

    AlreadyExists exception does not required error data

--- application/classes/Service/Account.php
+++ application/classes/Service/Account.php
@@ -31,7 +31,7 @@ class Service_Account extends Service
 
 		if ($account->loaded())
 		{
-			throw Service_Exception::factory('AlreadyExists', 'Account already exists')->data($validation['errors']);
+			throw Service_Exception::factory('AlreadyExists', 'Account already exists');
 		}
 
 		// Nothing wrong, save data
