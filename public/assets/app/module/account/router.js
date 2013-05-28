ca7bb102f1f77a13eb0f98848b15e80baa771511
Thomas Chemineau <thomas.chemineau@gmail.com>
2013-05-28 13:45:39 +0200

    account router can now handle login process

--- public/assets/app/module/account/router.js
+++ public/assets/app/module/account/router.js
@@ -1,7 +1,9 @@
 define([
+	'backbone',
 	'marionette',
-	'module/account/controller'
-], function(Marionette, AccountController)
+	'module/account/controller',
+	'module/account/model/account'
+], function(Backbone, Marionette, AccountController, AccountModel)
 {
 	var AccountRouter = Marionette.AppRouter.extend(
 	{
@@ -14,10 +16,26 @@ define([
 
 		initialize: function(options)
 		{
+			var App = options.app;
+
 			this.controller.initialize({
-				app: options.app
-			})
-		}
+				app: App
+			});
+
+			// On login succeed, fetch user and set it into the application
+			App.vent.on('login:success', function (data)
+			{
+				var account = new AccountModel(data);
+
+				account.fetch({
+					success: function ()
+					{
+						App.user = account;
+						Backbone.history.navigate('/#');
+					}
+				});
+			});
+		},
 	});
 
 	return AccountRouter;
