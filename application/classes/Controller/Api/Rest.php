f7043fb1dac3f213eea9e4714d5242f92df84cd5
Thomas Chemineau <thomas.chemineau@gmail.com>
2013-05-28 13:42:45 +0200

    fix issues when no response message is given

--- application/classes/Controller/Api/Rest.php
+++ application/classes/Controller/Api/Rest.php
@@ -15,7 +15,9 @@ abstract class Controller_Api_Rest extends RESTful_Controller
 	public function response ( $response = NULL, $code = 200 )
 	{
 		if (is_null($response))
-			$response = self::build_response();
+			$response = Service::factory('Api')->build_response_succeed();
+
+		$response['code'] = $code;
 
 		$this->response->headers('Content-Type', 'application/json');
 		$this->response->status($code);
--- application/classes/Controller/Api/Standard.php
+++ application/classes/Controller/Api/Standard.php
@@ -15,10 +15,13 @@ abstract class Controller_Api_Standard extends Controller
 	public function response ( $response = NULL, $code = 200 )
 	{
 		if (is_null($response))
-			$response = self::build_response();
+			$response = Service::factory('Api')->build_response_succeed();
+
+		$response['code'] = $code;
 
 		$this->response->headers('Content-Type', 'application/json');
 		$this->response->status($code);
+
 		$this->response->body(json_encode($response));
 	}
 
