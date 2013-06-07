597ae6f20ac4ffac1da1b14fb095b6bfc8bb6e46
thomas <thomas.chemineau@gmail.com>
2013-06-07 17:43:36 +0200

    backend application intercept url fragment

--- application/routes.php
+++ application/routes.php
@@ -5,13 +5,29 @@
  * defaults for the URI.
  */
 
+Route::set('account', 'account/confirm/<id>')
+	->defaults(array(
+		'action'     => 'confirm',
+		'controller' => 'Account'
+	));
+
+Route::set('api_v1_auth', 'api/v1/auth(/<action>)')
+	->defaults(array(
+		'action'     => 'index',
+		'controller' => 'Auth',
+		'directory'  => 'Api/V1'
+	));
+
 Route::set('api_v1', 'api/v1/(<controller>(/<id>))')
 	->defaults(array(
 		'directory'  => 'Api/V1'
 	));
 
-Route::set('default', '(<controller>(/<action>(/<id>)))')
+Route::set('default', '<fragment>',array(
+		'fragment' => '.*'
+	))
 	->defaults(array(
-		'controller' => 'welcome',
 		'action'     => 'index',
+		'controller' => 'welcome'
 	));
+
