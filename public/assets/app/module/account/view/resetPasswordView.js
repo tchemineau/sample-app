4ea38244621758491f5f34b3f3bf6b6f9fad5d09
thomas <thomas.chemineau@gmail.com>
2013-06-08 12:25:52 +0200

    add views and template for forget and reset password action

---
+++ public/assets/app/module/account/view/resetPasswordView.js
@@ -0,0 +1,139 @@
+
+define([
+	'marionette',
+	'marionette.formview',
+	'text!module/account/template/resetPasswordView.html'
+], function(Marionette, MarionetteFormView, ResetPasswordTemplate)
+{
+	var ResetPasswordView = Marionette.FormView.extend(
+	{
+		template: ResetPasswordTemplate,
+
+		/**
+		 * Declare variables to access template content
+		 */
+		ui: {
+			password: '#account-password',
+			password2: '#account-password2'
+		},
+
+		/**
+		 * Declare inputs
+		 */
+		fields: {
+		},
+
+		/**
+		 * What to do if an error occurs on the model
+		 */
+		onModelError: function (response, xhr)
+		{
+			this.showGlobalError(response.message);
+		},
+
+		/**
+		 * Save model when submit me
+		 */
+		onSubmit: function (evt)
+		{
+			evt.preventDefault();
+
+			// This store this object
+			var me = this;
+
+			// Get application
+			var app = this.options.app;
+
+			// Do the request to log the user
+			$.ajax(
+			{
+				url: 'api/v1/auth/forgot_password',
+				type: 'POST',
+				dataType: 'json',
+				data: {
+					'email': $('#account-email').val()
+				},
+				success: function (response)
+				{
+					me.showGlobalSuccess('You will soon receive an email giving you steps to reset your password');
+				},
+				error: function (xhr, status)
+				{
+					me.showGlobalError('Unable to send reset password instructions');
+				}
+			});
+		},
+
+		/**
+		 * What to do if the submit fails
+		 */
+		onSubmitFail: function (errors)
+		{
+			this.showLocalErrors(errors);
+		},
+
+		/**
+		 * Show an global alert
+		 */
+		showGlobalAlert: function (type, message, isClosable)
+		{
+			// Get alert container
+			var container = $('#account-alert-container');
+
+			// Create the alert box
+			var alertbox = $('<div class="alert alert-'+type+'" />');
+
+			// Include closable button if needed
+			if (typeof isClosable != 'boolean' || isClosable)
+				alertbox.append($('<button type="button" class="close" data-dismiss="alert">&times;</button>'));
+
+			// Append message
+			alertobx.append('<strong>Error</strong>: '+message);
+
+			// Show the message
+			alertbox.appendTo(container).alert();
+		},
+
+		/**
+		 * Show an error
+		 */
+		showGlobalError: function (message)
+		{
+			this.showGlobalAlert('error', message);
+		},
+
+		/**
+		 * Show success
+		 */
+		showGlobalSuccess: function (message)
+		{
+			this.showGlobalAlert('success', message, false);
+		},
+
+		/**
+		 * Highlight input in error
+		 */
+		showLocalErrors: function (errors)
+		{
+			var cpt = 0;
+
+			_(errors).each(function (field)
+			{
+				$('#account-'+field.el+'-control').addClass('error');
+				$('#account-'+field.el).tooltip({
+					placement: 'right',
+					title: field.error[0]}
+				);
+
+				if (cpt == 0)
+					$('#account-'+field.el).tooltip('show');
+
+				cpt++;
+			});
+		}
+
+	});
+
+	return ResetPasswordView;
+});
+
