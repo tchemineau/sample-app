dc98af10f5efa6a6f98b69f19730768430ffd972
thomas <thomas.chemineau@gmail.com>
2013-06-13 16:54:01 +0200

    add an endpoint to send forgot password instruction

--- application/classes/Controller/Api/V1/Auth.php
+++ application/classes/Controller/Api/V1/Auth.php
@@ -6,6 +6,36 @@
 class Controller_Api_V1_Auth extends Controller_Api_Standard
 {
 
+	public function action_forgot_password ()
+	{
+	       // Get request parameters
+	       $email = $this->request->post('email');
+
+	       // Build request data
+	       $data = array('email' => $email);
+
+	       // Get the account service
+	       $account_service = Service::factory('Account');
+
+	       // Get the api service
+	       $api_service = Service::factory('Api');
+
+	       try
+	       {
+	               // Send password reset instructions
+	               $account = $account_service->forgot_password($data);
+
+	               // Return appropriate HTTP result
+	               $this->response($api_service->build_response_succeed(
+	                       'Forgot password instruction sent'
+	               ), 200);
+	       }
+	       catch (Exception $e)
+	       {
+	               $this->response($api_service->build_response('Bad request'), 400);
+	       }
+	}
+
 	public function action_index ()
 	{
 		// Get request parameters
