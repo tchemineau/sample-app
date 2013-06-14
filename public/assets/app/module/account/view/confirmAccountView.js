b9fb7bc32f1c271a23b7b091435fb8f73f721b92
thomas <thomas.chemineau@gmail.com>
2013-06-14 18:26:14 +0200

    add confirm account view

---
+++ public/assets/app/module/account/view/confirmAccountView.js
@@ -0,0 +1,75 @@
+
+define([
+	'marionette',
+	'text!module/account/template/confirmAccountView.html'
+], function(Marionette, ConfirmAccountTemplate)
+{
+	var ConfirmAccountView = Marionette.ItemView.extend(
+	{
+		template: ConfirmAccountTemplate,
+
+		/**
+		 * Do stuff at initialization
+		 */
+		initialize: function (options)
+		{
+			var app = options.app;
+
+			this.checked = false;
+			this.confirmed = false;
+
+			app.vent.on('account:confirm', this.render, this);
+
+			this.confirmAccount();
+		},
+
+		/**
+		 * Confirm account
+		 */
+		confirmAccount: function ()
+		{
+			// This store this object
+			var me = this;
+
+			// Get application
+			var app = this.options.app;
+
+			// Do the request to log the user
+			$.ajax(
+			{
+				url: 'api/v1/auth/confirm_email',
+				type: 'POST',
+				dataType: 'json',
+				data: { 'token': me.options.token },
+				success: function (response)
+				{
+					me.checked = true;
+					me.confirmed = true;
+
+					app.vent.trigger('account:confirm');
+				},
+				error: function (xhr, status)
+				{
+					me.checked = true;
+					me.confirmed = false;
+
+					app.vent.trigger('account:confirm');
+				}
+			});
+		},
+
+		serializeData: function()
+		{
+			var app = this.options.app;
+
+			return {
+				checked: this.checked,
+				confirmed: this.confirmed,
+				url: app.url
+			};
+		}
+	});
+
+	return ConfirmAccountView;
+});
+
