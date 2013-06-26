bdcd543a97f0587c311d9d87e4622859ade74a12
thomas <thomas.chemineau@gmail.com>
2013-06-26 15:03:52 +0200

    add a local configuration for mongodb

--- application/config/mongodb.php
+++ application/config/mongodb.php
@@ -1,6 +1,10 @@
 <?php defined('SYSPATH') or die();
 
-return array(
+/**
+ * !! This is default settings.
+ * !! To configure your settings, please do it into app.local.php
+ */
+$config = array(
 	'default' => array(
 		'host' => 'localhost',
 		'port' => 27017,
@@ -9,3 +13,19 @@ return array(
 		'password' => '',
 	),
 );
+
+/**
+ * !! This will load local settings.
+ */
+if (file_exists(APPPATH.'config/mongodb.local.php'))
+{
+	require_once APPPATH.'config/mongodb.local.php';
+	$config = array_merge($config, $local);
+	unset($local);
+}
+
+/**
+ * Return configuration
+ */
+return $config;
+
