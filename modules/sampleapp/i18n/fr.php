e592c3d5f49b7732afe77484630cbe36efd8ae95
Thomas Chemineau <thomas.chemineau@gmail.com>
2013-08-14 12:01:51 +0200

    add a first version of fr language

---
+++ modules/sampleapp/i18n/fr.php
@@ -0,0 +1,16 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+return array(
+
+	'My Account' => 'Mon Compte',
+	'or' => 'ou',
+	'Sign In' => 'Se connecter',
+	'Sign Off' => 'Se déconnecter',
+	'Sign Up' => 'Créer un compte',
+	'This is a demo application using :kohana with REST, :mongodb, :backbonejs and :marionettejs.' =>
+		'C\'est application de démonstration utilisant :kohana avec REST, :mongodb, :backbonejs et :marionettejs.',
+	'View the project on GitHub' => 'Voir le projet sur GitHub',
+	'Welcome on :title' => 'Bienvenue sur :title',
+
+);
+
