04dbb9797af73105898b889bb1d20b3062b18eb6
Thomas Chemineau <thomas.chemineau@gmail.com>
2013-08-19 16:35:07 +0200

    internationalize emails

--- modules/sampleapp/i18n/fr.php
+++ modules/sampleapp/i18n/fr.php
@@ -15,9 +15,11 @@ return array(
 		'En cliquant sur "Réinitialiser mon mot de passe", votre mot de passe sera réinitialisé et ce lien ne fonctionnera plus.',
 
 	'By clicking on "Send" below, you will receiving an email which gives you steps to reset your password.' =>
-		'En cliquant sur "Envoyer", vous allez recevoir un e-mail vous indiquant les étapes de réinitialisation de votre mot de passe',
+		'En cliquant sur "Envoyer", vous allez recevoir un e-mail vous indiquant les étapes de réinitialisation de votre mot de passe.',
 
 	'Change your avatar at Gravatar.com' => 'Changez votre avatar sur Gravatar.com',
+
+	'Confirmation sent from' => 'Confirmation envoyée par',
 
 	'Confirm your password' => 'Confirmer votre mot de passe',
 
@@ -30,6 +32,8 @@ return array(
 	'Firstname' => 'Prénom',
 
 	'Gravatar email' => 'E-mail Gravatar',
+
+	'Hey' => 'Bonjour',
 
 	'I don\'t have account' => 'Je n\'ai pas de compte',
 
@@ -47,6 +51,12 @@ return array(
 	'or' => 'ou',
 
 	'Oups' => 'Aie',
+
+	'Please confirm your email address by clicking the following link and you\'re ready to go:' =>
+		'Veuillez confirmer votre adresse e-mail en cliquant sur le lien suivant, votre inscription sera alors terminée:',
+
+	'Please note that this link will expire in three hours from the time it was sent.' =>
+		'Veuillez noter que ce lien expirera trois heures après l\'envoi de cet e-mail.',
 
 	'Please sign in' => 'Connexion',
 
@@ -70,29 +80,45 @@ return array(
 
 	'Sign Up' => 'Créer un compte',
 
+	'Thank you for using :title, we hope you will be back soon!' =>
+		'Merci d\'avoir utilisé :title, nous espérons vous revoir prochainement!',
+
+	'The :title team.' => 'L\'équipe :title.',
+
 	'This action cannot be undone. This will delete your account permanently.' =>
 		'Cette action ne peut pas être annulée. Votre compte sera définitivement supprimé.',
 
 	'This is a demo application using :kohana with REST, :mongodb, :backbonejs and :marionettejs.' =>
 		'Une application de démonstration utilisant :kohana avec REST, :mongodb, :backbonejs et :marionettejs.',
 
+	'This is an automated message. Please, do not reply.' =>
+		'Ceci est un message automatique. Veuillez ne pas répondre.',
+
 	'View the project on GitHub' => 'Voir le projet sur GitHub',
 
 	'We are not able to confirm your account.' =>
 		'Votre compte n\'a pas pu être confirmé',
+
+	'Welcome on board!' => 'Bienvenue à bord !',
 
 	'Welcome on :title' => 'Bienvenue sur :title',
 
 	'Yes, I want to delete my account now' =>
 		'Oui, je veux supprimer mon compte maintenant',
 
+	'Your are now able to reset your password by clicking this following link:' =>
+		'Vous pouvez maintenant réinitialiser votre mot de passe en cliquant sur le lien suivant:',
+
 	'You are now allowed to sign in with your account on our services.' =>
 		'Vous pouvez maintenant vous connecter avec votre compte à nos services.',
 
 	'You will soon receive an email giving you steps to reset your password.' =>
-		'Vous allez bientôt recevoir un e-mail vous indiquant les étapes de réinitialisation de votre mot de passe',
+		'Vous allez bientôt recevoir un e-mail vous indiquant les étapes de réinitialisation de votre mot de passe.',
 
 	'Your account has been confirmed' => 'Vous avez confirmé votre compte',
+
+	'Your account has been permanently deleted from :title.' =>
+		'Votre compte a été supprimé définitivement de :title.',
 
 	'Your email' => 'Votre e-mail',
 
@@ -108,6 +134,9 @@ return array(
 
 	'Your registration is almost complete. Please, check your mailbox to confirm your new account.' =>
 		'Votre inscription est bientôt terminée. Veuillez vérifier vos e-mails afin de confirmer votre nouveau compte.',
+
+	'Your registration to :title is almost complete.' =>
+		'Votre inscription à :title est bientôt terminée.',
 
 );
 
