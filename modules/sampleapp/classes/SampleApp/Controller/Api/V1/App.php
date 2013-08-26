2c341604dca709abbc65f96c15ca8e6f5883a57d
Thomas Chemineau <thomas.chemineau@gmail.com>
2013-08-26 12:16:17 +0200

    require_js endpoint returns correct HTTP status if error occurs

--- modules/sampleapp/classes/SampleApp/Controller/Api/V1/App.php
+++ modules/sampleapp/classes/SampleApp/Controller/Api/V1/App.php
@@ -14,41 +14,35 @@ class SampleApp_Controller_Api_V1_App extends Controller_Api_Standard
 		// Get the action
 		$route = $this->request->post('route');
 
-		// Get the application service
-		$app_service = Service::factory('Application');
-
 		// Get the api service
 		$api_service = Service::factory('Api');
 
-		if (!is_null($route))
+		// If no route is given, it is a bad request
+		if (is_null($route))
+			return $this->response($api_service->build_response_failed('Bad request'), 400);
+
+		// Get the application service
+		$app_service = Service::factory('Application');
+
+		// Store modules to load
+		$modules = array();
+
+		// Get JS to load
+		switch ($route)
 		{
-			// Store modules to load
-			$modules = array();
-
-			// Store return message
-			$message = 'Modules not found';
-
-			// Get JS to load
-			switch ($route)
-			{
-				case 'init':
-					$modules = $app_service->get_js_init();
-					break;
-				default:
-					$modules = $app_service->get_js_path($route);
-			}
-
-			// Set a correct message
-			if (sizeof($modules) > 0)
-				$message = 'Modules found';
-
-			// Return appropriate HTTP result
-			$this->response($api_service->build_response_succeed($message, $modules), 200);
+			case 'init':
+				$modules = $app_service->get_js_init();
+				break;
+			default:
+				$modules = $app_service->get_js_path($route);
 		}
-		else
-		{
-			$this->response($api_service->build_response_failed('Bad request'), 400);
-		}
+
+		// Set a correct message
+		if (sizeof($modules) > 0)
+			return $this->response($api_service->build_response_succeed('Modules found', $modules), 200);
+
+		// In other way, return a 404 not found
+		$this->response($api_service->build_response_failed('Modules not found'), 404);
 	}
 
 } // End SampleApp_Controller_Api_V1_App
