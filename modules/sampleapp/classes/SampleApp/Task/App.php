c3f3b1e4731032e29360228e4ef8e40be447a4ee
Thomas Chemineau <thomas.chemineau@gmail.com>
2013-10-09 11:53:27 +0200

    move code from job class to app class

--- modules/sampleapp/classes/SampleApp/Task/App.php
+++ modules/sampleapp/classes/SampleApp/Task/App.php
@@ -23,5 +23,33 @@ abstract class SampleApp_Task_App extends Minion_Task
 		);
 	}
 
+	/**
+	 * Sets options for this task
+	 *
+	 * $param  array  the array of options to set
+	 * @return this
+	 */
+	public function set_options ( array $options )
+	{
+		// Call parent method
+		parent::set_options($options);
+
+		// Now parse all options and decode values
+		foreach ($this->_options as $k => $v)
+		{
+			// Try to convert into array
+			$value = $v != '' ? explode(',', $v) : NULL;
+
+			// Do not store array if only one value
+			if (!is_null($value) and sizeof($value) == 1)
+				$value = $value[0];
+
+			// Reassign the value
+			$this->_options[$k] = $value;
+		}
+
+		return $this;
+	}
+
 } // End SampleApp_Task_App
 
